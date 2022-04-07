c59eaa0fd79e7b1f9aaaa27ef95b434265fc9604
markgietl <markgietl4@gmail.com>
2022-04-07 20:07:19 +0200

    Deploy React app to GitHub Pages

---
+++ precache-manifest.b2460956c665abe1d2a3d2870c8aefef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "158273fa0837c0581575",
+    "url": "/static/css/main.923c2a67.chunk.css"
+  },
+  {
+    "revision": "158273fa0837c0581575",
+    "url": "/static/js/main.158273fa.chunk.js"
+  },
+  {
+    "revision": "717fe7bbea06892888fe",
+    "url": "/static/js/1.717fe7bb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "90938ccec305b319a4e1e94c2297c003",
+    "url": "/index.html"
+  }
+];
