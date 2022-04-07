f80aa62c9b2beb116f2dfc7f28906943d3b4ce2f
markgietl <markgietl4@gmail.com>
2022-04-07 16:50:12 +0200

    Built site in React

--- markgietl/src/components/Intro.js
+++ markgietl/src/components/Intro.js
@@ -4,7 +4,7 @@ import './Intro.css'
 function Intro() {
   return (
     <div className="intro-container">
-        <div><h1 className="heading">A glimpse into my software development journey!</h1></div>
+        <div><h1 className="headingIntro">A glimpse into my software development journey!</h1></div>
         <div><img className="myPhoto" src='/images/me.jpg' alt='me'></img></div>
     </div>
   )
